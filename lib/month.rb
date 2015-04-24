6620fe19ff45ef354f7c122c07eedc8a43c2123b
Margaret Clinard <margaret.clinard@gmail.com>
2015-04-24 09:47:41 -0500

    Implemented day_count method. Reorganized order of methods.

--- lib/month.rb
+++ lib/month.rb
@@ -8,6 +8,26 @@ class Month
 
   WEEKDAYS = "Su Mo Tu We Th Fr Sa"
 
+  def name
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    months[@month - 1]
+  end
+
+  def day_count
+    months_with_30 = [9, 4, 6, 11]
+    months_with_31 = [1, 3, 5, 7, 8, 12]
+
+    if months_with_30.include? @month
+      30
+    elsif months_with_31.include? @month
+      31
+    elsif (@year % 4 == 0) && !(@year % 100 == 0) || (@year % 400 == 0)
+      29
+    else
+      28
+    end
+  end
+
   def to_s
     <<EOS
     #{name} #{year}
@@ -21,8 +41,4 @@ class Month
 EOS
   end
 
-  def name
-    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    months[@month - 1]
-  end
 end
