17f4635db136606d5c3d079569e923e966b495a1
Margaret Clinard <margaret.clinard@gmail.com>
2015-04-27 12:56:43 -0500

    Put leap year logic in its own method.

--- lib/month.rb
+++ lib/month.rb
@@ -16,6 +16,10 @@ class Month
     months[@month - 1]
   end
 
+  def leap_year?
+    (@year % 4 == 0) && !(@year % 100 == 0) || (@year % 400 == 0)
+  end
+
   def day_count
     months_with_30 = [9, 4, 6, 11]
     months_with_31 = [1, 3, 5, 7, 8, 10, 12]
@@ -24,7 +28,7 @@ class Month
       30
     elsif months_with_31.include? @month
       31
-    elsif (@year % 4 == 0) && !(@year % 100 == 0) || (@year % 400 == 0)
+    elsif leap_year?
       29
     else
       28
