e6f3a922efbb3a104858a43a9412b650c364b203
Margaret Clinard <margaret.clinard@gmail.com>
2015-04-29 14:11:03 -0500

    Added logic to call year in cal.rb

--- cal.rb
+++ cal.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/env ruby
 
 require_relative 'lib/month'
-
-month = ARGV[0].to_i
-year = ARGV[1].to_i
+require_relative 'lib/year'
 
 def year_range_error
   puts <<EOS
@@ -12,9 +10,22 @@ Date not in acceptable format/range.
 EOS
 end
 
-if year > 3000 || year < 1800
-  year_range_error
-else
-  m = Month.new(month, year)
-  puts m.to_s
+
+if ARGV.length == 2
+  month = ARGV[0].to_i
+  year = ARGV[1].to_i
+
+  if year > 3000 || year < 1800
+    year_range_error
+  else
+    m = Month.new(month, year)
+    puts m.to_s
+  end
+
+elsif ARGV.length == 1
+  year = ARGV[0].to_i
+
+  y = Year.new(year)
+  puts y.display
 end
+
